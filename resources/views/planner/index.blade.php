ea9470b0e2383f2414bea37b4d2da59f2baa2c83
Emma Lane <emma-louise-lane@outlook.com>
2025-08-10 19:01:55 +0100

    Amend: Update to use more bladewind buttons

--- resources/views/planner/index.blade.php
+++ resources/views/planner/index.blade.php
@@ -67,8 +67,7 @@
     }
     
     .todo-actions {
-        display: flex;
-        gap: 0.5rem;
+        margin: auto;
     }
     
     .add-todo-form {
@@ -353,22 +352,13 @@
                         </div>
                     </div>
                     <div class="todo-actions">
-                        <button onclick="editTodo({{ $todo->id }})" 
-                                class="p-1 text-gray-500 hover:text-primary-600">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                            </svg>
-                        </button>
+                        <x-bladewind::button.circle icon="pencil" class="size-2" outline="true" size="tiny"
+                                                    onclick="editTodo({{ $todo->id }})" />
+                        
                         <form action="{{ route('planner.destroy', $todo) }}" method="POST" class="inline">
                             @csrf
                             @method('DELETE')
-                            <button type="submit" 
-                                    class="p-1 text-gray-500 hover:text-red-600"
-                                    onclick="return confirm('Are you sure you want to delete this task?')">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                </svg>
-                            </button>
+                            <x-bladewind::button.circle icon="trash" class="size-2" outline="true" size="tiny" can_submit="true" />
                         </form>
                     </div>
                 </div>
