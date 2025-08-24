23bb170a7b9f8942b9f18769bdea852e03c3c61b
Emma Lane <emma-louise-lane@outlook.com>
2025-08-24 23:52:47 +0100

    IMP Font update to Poiret One

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,9 @@
     <link rel="apple-touch-icon" href="{{ asset('images/profile-pic.png') }}">
     
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Josefin+Slab:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Edu+NSW+ACT+Cursive:wght@400..700&family=Poiret+One&display=swap" rel="stylesheet">
     
     <!-- Alpine.js -->
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
