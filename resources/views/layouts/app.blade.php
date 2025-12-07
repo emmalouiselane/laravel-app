a38e4581ade5ecd8b236d2ad0a1f1b840a2bac34
Emma Lane <emma-louise-lane@outlook.com>
2025-12-07 00:28:12 +0000

    IMP Add github link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,25 +1,27 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="robots" content="noindex, nofollow, noarchive, nosnippet, noodp, noimageindex, notranslate, nocache">
     <title>@yield('title', config('app.name', 'Laravel'))</title>
-    
+
     <!-- Favicon -->
     <link rel="icon" href="{{ asset('images/profile-pic.png') }}" type="image/png">
     <link rel="shortcut icon" href="{{ asset('images/profile-pic.png') }}" type="image/png">
     <link rel="apple-touch-icon" href="{{ asset('images/profile-pic.png') }}">
-    
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
-    
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
+        rel="stylesheet">
+
     <!-- Alpine.js -->
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
-    
+
     <!-- BladewindUI CSS -->
     <link href="{{ asset('vendor/bladewind/css/animate.min.css') }}" rel="stylesheet" />
     <link href="{{ asset('vendor/bladewind/css/bladewind-ui.min.css') }}" rel="stylesheet" />
@@ -28,41 +30,53 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <script src="https://tinylytics.app/embed/ZqiXX112qeb4C8pdyGQg.js" defer></script>
-    
+
     @stack('styles')
 </head>
+
 <body class="antialiased flex flex-col h-screen">
     <main class="flex-grow">
         @yield('content')
     </main>
-    
+
     <script src='https://storage.ko-fi.com/cdn/scripts/overlay-widget.js'></script>
     <script>
-    kofiWidgetOverlay.draw('sparklane', {
-        'type': 'floating-chat',
-        'floating-chat.donateButton.text': 'Support Me',
-        'floating-chat.donateButton.background-color': '#00bfa5',
-        'floating-chat.donateButton.text-color': '#fff'
-    });
+        kofiWidgetOverlay.draw('sparklane', {
+            'type': 'floating-chat',
+            'floating-chat.donateButton.text': 'Support Me',
+            'floating-chat.donateButton.background-color': '#00bfa5',
+            'floating-chat.donateButton.text-color': '#fff'
+        });
     </script>
 
     <footer class="bg-gray-100 py-4 mt-auto">
-        <div class="container text-right text-gray-600 text-sm pr-5" style="line-height: 46px;">
-            Version {{ env('APP_RELEASE', '1.0.0') }}
+        <div class="container flex justify-end pr-5" style="line-height: 46px;">
+            <div class="text-gray-600 text-sm mt-2">Version {{ env('APP_RELEASE', '1.0.0') }}</div>
+            <div>
+                <a target="_blank" rel="noopener noreferrer" title="GitHub"
+                    href="https://github.com/emmalouiselane/laravel-app"
+                    class="ml-2 mt-2 text-gray-600 hover:text-gray-900 inline-flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="currentColor" viewBox="0 0 24 24">
+                        <path
+                            d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                    </svg>
+                </a>
+            </div>
         </div>
     </footer>
-    
+
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    
+
     <!-- Select2 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <link href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" rel="stylesheet" />
-    
+    <link href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css"
+        rel="stylesheet" />
+
     <!-- BladewindUI JS -->
     <script src="{{ asset('vendor/bladewind/js/helpers.js') }}"></script>
     <script src="{{ asset('vendor/bladewind/js/bladewind.js') }}"></script>
-    
+
     <!-- Select2 JS -->
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script>
@@ -73,4 +87,5 @@
     </script>
     @stack('scripts')
 </body>
+
 </html>
